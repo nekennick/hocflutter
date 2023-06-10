20f7c3e7e7142a0f579773d5fdd9bcdb617d9a08
Khoa <nekennick@gmail.com>
2023-06-10 22:45:59 +0700

    ok

--- baoflutter/lib/main.dart
+++ baoflutter/lib/main.dart
@@ -1,7 +1,64 @@
-import 'dart:io';
+import 'package:flutter/material.dart';
+
+const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
 
 void main() {
-  print("Nhập Exit để thoát game");
-  var name = stdin.readLineSync();
-  print('chào $name đến');
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: darkBlue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: MyWidget(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'Hello, World!',
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+        Row(
+          children: [
+            ElevatedButton(
+                child: Text('RED'),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.red)),
+                onPressed: () {}),
+            ElevatedButton(
+                child: Text('YELLOW'),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.yellow)),
+                onPressed: () {}),
+            ElevatedButton(
+                child: Text('GREEN'),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.green)),
+                onPressed: () {}),
+            ElevatedButton(
+                child: Text('BLUE'),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.blue)),
+                onPressed: () {}),
+          ],
+        )
+      ],
+    );
+  }
 }
